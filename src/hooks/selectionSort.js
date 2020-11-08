e3ec006d3b617e728fdb2ac814db16b8e63a85fe
S F <sebastian.fariasb@usach.cl>
2020-11-08 17:51:10 -0300

    Selection Sort refactorized

--- src/hooks/selectionSort.js
+++ src/hooks/selectionSort.js
@@ -1,7 +1,8 @@
 const selectionSort = {
-  initial: (size, generate) => {
-    return { 
-      array: generate(size),
+  initial: generated => {
+    return {
+      algo: 'selectionSort',
+      array: generated,
       minIndex: 0,
       i: 0,
       j: 1,
@@ -9,44 +10,55 @@ const selectionSort = {
   },
   step: ({array , i, j, minIndex}, setBars, run, speed) => {
     if(run && i < array.length) setTimeout( () => {
-      const newArray = [...array]
+      let newArray = [...array]
       const n = newArray.length
       let newI = i
       let newJ = j
       let newMinIndex = minIndex    
-      if(j == i+1 ) newArray[n-1].state = 'new'
-      newArray[j].state = 'inspecting'
-      newArray[j-1].state = newArray[j-1].state === 'inspecting'? 'new' : newArray[j-1].state
-      if(newArray[minIndex].heigth > newArray[j].heigth){
-        newArray[minIndex].state = 'new'
-        newArray[j].state = 'selected'
+      if(newJ < n && newArray[minIndex].heigth > newArray[j].heigth){
         newMinIndex = j
       }
-      if(newJ + 1 >= n){
-        const temp = newArray[newMinIndex].heigth
-        newArray[newMinIndex] = { heigth: newArray[i].heigth, state: 'new'}
-        newArray[i] = {heigth: temp, state: 'sorted'}
+      newArray = defaultColors(newArray,newMinIndex,newI,newJ)
+      if(newJ >= n){
+        newArray = swapColors(newArray,newMinIndex,newI)
         newI++
         newMinIndex = newI
-        if(newI < n){
-          newArray[newI].state = 'selected'
-        }else{
-          newArray[newI-1].state = 'sorted'
-        }
         newJ = newI + 1
       }else{
         newJ++
       }
-      if(newI + 1 >= n){
-        newJ = newI  
-      }
       setBars({ 
+        algo: 'selectionSort',
         array: newArray,
         minIndex: newMinIndex,
         i: newI,
         j: newJ,
       })
-    } 
-    ,speed)}}
-
-    export default selectionSort
+    },speed)
+  },
+}
+const defaultColors = (array, selected, i, j) => {
+  const n = array.length
+  return array.map( (bar,index) => {
+    bar.state = index < i ? 'sorted' : 'new'
+    if(index == j) bar.state = 'inspecting'
+    if( index == selected ) bar.state = 'selected'
+    return bar
+  })
+}
+const swapColors = (array, selected, i) => {
+    return swap(array,selected,i).map( (bar,index) => {
+      bar.state = index <= i ? 'sorted' : 'new'
+      if( index == i+1) bar.state = 'inspecting'
+      return bar
+    })
+}
+const swap = (array,selected,i) => {
+  const temp = array[i].heigth
+  return array.map( (bar,index) => {
+    if(index == i) bar.heigth = array[selected].heigth 
+    if(index == selected) bar.heigth = temp
+    return bar    
+  })
+}
+export default selectionSort
