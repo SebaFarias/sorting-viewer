65e3bbd1d89452b6b51b127e35979d18bd4dff42
S F <sebastian.fariasb@usach.cl>
2020-11-08 16:18:46 -0300

    Bubble Sort refactored

--- src/hooks/bubbleSort.js
+++ src/hooks/bubbleSort.js
@@ -1,48 +1,53 @@
 const bubbleSort = {
-  initial: (size, generate) => {
+  initial: generated => {
     return {
-      array: generate(size),
+      algo: 'bubbleSort',
+      array: generated,
       i: 0,
       j: 0,
     }
   } ,
   step: ({array , i, j}, setBars, run, speed) => {
-    if(run && i + 1 < array.length ) setTimeout( () => {
-      console.log(j)
-      const newArray = [...array]
-      const n = newArray.length
+    if(run && i < array.length ) setTimeout( () => {      
+      const n = array.length
+      let newArray = [...array]
       let newI = i
-      let newJ = j
-      newArray.map( (bar,index) => {
-        bar.state = 'new'
-        if(index > n - newI - 1) bar.state = 'sorted'
-      })
-      newArray[j].state = 'selected'
-      if(newArray[j].heigth > newArray[j+1].heigth){
-        const temp = { heigth: newArray[j].heigth , state: 'selected'}
-        newArray[j] = { heigth: newArray[j+1].heigth , state: 'new'}
-        newArray[j+1] = temp
-      }else{
-        newArray[j].state = 'new'
-        newArray[j+1].state = 'selected'
+      let newJ = j      
+      if( i +  1 < n && array[j].heigth > array[j+1].heigth){
+        newArray = swap(newArray,newJ)
       }
+      newArray = defaultColors(newArray,newI,newJ)
       if(newJ + newI + 2 >= n){
         newI++
         newJ = 0
       }else{
         newJ++
       }
-      if(newI + 1 >= n){
-        newArray[0].state = 'sorted'
-        newArray[1].state = 'sorted'
-      }
       setBars({ 
+        algo: 'bubbleSort',
         array: newArray,
         i: newI,
         j: newJ,
       })
     },speed)
   },
+}
+const defaultColors = (array,i,j) => {
+  const n = array.length
+  return array.map( (bar,index) => {
+    bar.state = index > n - i - 1 ? 'sorted' : 'new'
+    if(index == j+1) bar.state = i+1 < n? 'selected' :'sorted'
+    if(i+1 >= n && index == 0 ) bar.state = 'sorted'
+    return bar
+  })
+}
+const swap = (array,j) => {
+  const temp = array[j].heigth
+  return array.map( (bar,index) => {
+    if(index == j) bar.heigth = array[j+1].heigth
+    if(index == j + 1) bar.heigth = temp
+    return bar    
+  })
 }
 
 export default bubbleSort
